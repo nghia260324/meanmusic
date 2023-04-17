8c21814d991506560670df222ccb13636a8e0051
nghia260324 <128986679+nghia260324@users.noreply.github.com>
2023-04-17 23:05:36 +0700

    Add files via upload

---
+++ assets/js/music-list.js
@@ -0,0 +1,132 @@
+// To add more song, just copy the following code and paste inside the array
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+//paste it inside the array as more as you want music then you don't need to do any other thing
+let allMusic = [
+  {
+    name: "1. WHY DO I FT. SWEET BUT PSYCHO",
+    artist: "Mean Music",
+    img: "https://avatar-nct.nixcdn.com/song/2023/01/05/c/a/8/d/1672904050958.jpg",
+    src: "song-1"
+  },
+  {
+    name: "2. Wehenkend x Ngẫu Hứng",
+    artist: "Mean Music",
+    img: "./assets/img/site.png",
+    src: "song-2"
+  },
+  {
+    name: "3. The Ocean ft Walk Thru Fire",
+    artist: "Thereon Remix",
+    img: "https://i.ytimg.com/vi/T7HyxZNzi7s/maxresdefault.jpg",
+    src: "song-3"
+  },
+  {
+    name: "4. Don't Hurt",
+    artist: "Mike Williams",
+    img: "https://i1.sndcdn.com/artworks-000221613575-v10ura-t500x500.jpg",
+    src: "song-4"
+  },
+  {
+    name: "5. Nevada",
+    artist: "Vicetone",
+    img: "https://i.ytimg.com/vi/ycOm6wFxMhQ/maxresdefault.jpg",
+    src: "song-5"
+  },
+  {
+    name: "6. RetroVision & Domastic - SICC",
+    artist: "NCS Release",
+    img: "https://i1.sndcdn.com/artworks-000222434012-2yqck6-t500x500.jpg",
+    src: "song-6"
+  },
+  {
+    name: "7. JJD - Aeon",
+    artist: "JJD",
+    img: "https://i.scdn.co/image/ab67616d0000b27326ce0d1f40f6867bf49ea36c",
+    src: "song-7"
+  },
+  {
+    name: "8. Dreams Remix - Lost Sky",
+    artist: "Zoverze Remix",
+    img: "https://i1.sndcdn.com/artworks-8smGArYyZ8LCCwd5-pzin7w-t500x500.jpg",
+    src: "song-8"
+  },
+  {
+    name: "9. Dream Chaos - Fantasize",
+    artist: "Dream Chaos",
+    img: "https://i1.sndcdn.com/artworks-hBoFe0zPJqRK5lXB-YFVaqA-t500x500.jpg",
+    src: "song-9"
+  },
+  {
+    name: "10. Ray Staller - Diamond",
+    artist: "Illumi Music",
+    img: "https://geo-media.beatport.com/image_size/1400x1400/d8cba7d3-d983-4bbd-9a2c-fea661849b0b.jpg",
+    src: "song-10"
+  },
+];
+
+let mainMusic = [
+  {
+    name: "WHY DO I FT. SWEET BUT PSYCHO",
+    artist: "Mean Music",
+    img: "https://avatar-nct.nixcdn.com/song/2023/01/05/c/a/8/d/1672904050958.jpg",
+    src: "song1"
+  },
+  {
+    name: "Wehenkend x Ngẫu Hứng",
+    artist: "Mean Music",
+    img: "./assets/img/site.png",
+    src: "song2"
+  },
+  {
+    name: "The Ocean ft Walk Thru Fire",
+    artist: "Thereon Remix",
+    img: "https://i.ytimg.com/vi/T7HyxZNzi7s/maxresdefault.jpg",
+    src: "song3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  
+];
+let chillMusic = [
+  {
+    name: "William Black - Deep Blue",
+    artist: "William Black",
+    img: "",
+    src: ""
+  },
+
+];
+
