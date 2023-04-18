877a930ff13f0ba8e5e4735c4569000d836015f5
nghia260324 <128986679+nghia260324@users.noreply.github.com>
2023-04-18 11:14:15 +0700

    Update music-list.js

--- assets/js/music-list.js
+++ assets/js/music-list.js
@@ -11,61 +11,61 @@ let allMusic = [
     name: "1. WHY DO I FT. SWEET BUT PSYCHO",
     artist: "Mean Music",
     img: "https://avatar-nct.nixcdn.com/song/2023/01/05/c/a/8/d/1672904050958.jpg",
-    src: "song-1"
+    src: "song1"
   },
   {
     name: "2. Wehenkend x Ngẫu Hứng",
     artist: "Mean Music",
     img: "./assets/img/site.png",
-    src: "song-2"
+    src: "song2"
   },
   {
     name: "3. The Ocean ft Walk Thru Fire",
     artist: "Thereon Remix",
     img: "https://i.ytimg.com/vi/T7HyxZNzi7s/maxresdefault.jpg",
-    src: "song-3"
+    src: "song3"
   },
   {
     name: "4. Don't Hurt",
     artist: "Mike Williams",
     img: "https://i1.sndcdn.com/artworks-000221613575-v10ura-t500x500.jpg",
-    src: "song-4"
+    src: "song4"
   },
   {
     name: "5. Nevada",
     artist: "Vicetone",
     img: "https://i.ytimg.com/vi/ycOm6wFxMhQ/maxresdefault.jpg",
-    src: "song-5"
+    src: "song5"
   },
   {
     name: "6. RetroVision & Domastic - SICC",
     artist: "NCS Release",
     img: "https://i1.sndcdn.com/artworks-000222434012-2yqck6-t500x500.jpg",
-    src: "song-6"
+    src: "song6"
   },
   {
     name: "7. JJD - Aeon",
     artist: "JJD",
     img: "https://i.scdn.co/image/ab67616d0000b27326ce0d1f40f6867bf49ea36c",
-    src: "song-7"
+    src: "song7"
   },
   {
     name: "8. Dreams Remix - Lost Sky",
     artist: "Zoverze Remix",
     img: "https://i1.sndcdn.com/artworks-8smGArYyZ8LCCwd5-pzin7w-t500x500.jpg",
-    src: "song-8"
+    src: "song8"
   },
   {
     name: "9. Dream Chaos - Fantasize",
     artist: "Dream Chaos",
     img: "https://i1.sndcdn.com/artworks-hBoFe0zPJqRK5lXB-YFVaqA-t500x500.jpg",
-    src: "song-9"
+    src: "song9"
   },
   {
     name: "10. Ray Staller - Diamond",
     artist: "Illumi Music",
     img: "https://geo-media.beatport.com/image_size/1400x1400/d8cba7d3-d983-4bbd-9a2c-fea661849b0b.jpg",
-    src: "song-10"
+    src: "song10"
   },
 ];
 
